9ce9cbcc1485d81bab39882045e5d963b99aefab
Filipe <filipecensi@hotmail.com>
2025-06-20 02:57:58 -0300

    Criando o gerenciamento de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useNavigate,
+    Outlet,
+} from "react-router-dom";
+
 import App from './App';
+import SinglePokemon from './components/SinglePokemon/SinglePokemon';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />}/>
+        <Route path='/pokemon/:name' element={<SinglePokemon />}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );

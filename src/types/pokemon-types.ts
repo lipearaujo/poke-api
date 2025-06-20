fdeef67cb0a76dd5f60507885ff27dc39d4f736a
Filipe <filipecensi@hotmail.com>
2025-06-20 02:58:40 -0300

    Criando type para a listagem de pokemons

---
+++ src/types/pokemon-types.ts
@@ -0,0 +1,10 @@
+export interface Pokemon {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: { type: { name: string } }[];
+  abilities: { ability: { name: string } }[];
+  base_experience: string
+  sprites: { [key: string]: string | undefined };
+}
